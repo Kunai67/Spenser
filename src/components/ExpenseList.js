ac83ed978005813528aea993fd45d3be301672cc
Ian Agustin <inbagustin@gmail.com>
2020-01-17 17:36:21 +0800

    Created expenseMockData.json

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ExpenseList() {
+    return (
+        <div>
+            <p>Total: </p>
+            <p></p>
+        </div>
+    )
+}
+
+export default ExpenseList
