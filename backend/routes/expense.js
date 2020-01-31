2a60341facb9a8172c975b6923e7f259db02d314
Ian Agustin <inbagustin@gmail.com>
2020-01-31 19:01:49 +0800

    DELETE and UPDATE routes done

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -4,7 +4,7 @@ const Expense = require('../models/expense.models');
 
 Router.route('/').get((req, res) => {
     Expense.find()
-        .then(expenses => res.json(expenses))
+        .then(expense => res.json(expense))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
@@ -17,6 +17,26 @@ Router.route('/add').post((req, res) => {
 
     newExpense.save()
         .then(() => res.json('Expense Added Succesfully'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+Router.route('/:id').delete((req, res) => {
+    Expense.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Expense deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
+Router.route('/update/:id').post((req, res) => {
+    Expense.findById(req.params.id)
+        .then(expense => {
+        expense.name = req.body.name;
+        expense.cost = req.body.cost;
+        expense.tags = req.body.tags;
+
+        expense.save()
+            .then(() => res.json('Exercise updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
