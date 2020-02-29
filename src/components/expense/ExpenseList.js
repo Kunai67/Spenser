e5f2f87e829939059e70798818ca186b0b28bb17
Ian Agustin <inbagustin@gmail.com>
2020-02-29 12:25:32 +0800

    Updated ExpenseList styling

--- src/components/expense/ExpenseList.js
+++ src/components/expense/ExpenseList.js
@@ -1,5 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const StyledTable = styled.table`
+    border: 1px solid #0b4f6c;
+    border-radius: 10px;
+    border-spacing: 0;
+    text-align: center;
+    width: 80%;
+    margin: 1rem auto;
+    overflow: hidden;
+
+    & tr:last-child td {
+        border: 0;
+    }
+`;
+
+const StyledHeadingRow = styled.tr`
+    background: #0b4f6c;
+`;
+
+const StyledHeadingCell = styled.th`
+    color: #FBFBFF;
+    font-family: 'Roboto';
+    font-weight: 700;
+    letter-spacing: 1.1px;
+    padding: 1rem;
+`;
+
+const StyledCell = styled.td`
+    color: #333;
+    font-family: 'Roboto Condensed';
+    padding: .5rem;
+    border-bottom: 1px solid #0b4f6c;
+`;
 
 export default class ExpenseList extends Component {
     constructor(props) {
@@ -14,24 +48,24 @@ export default class ExpenseList extends Component {
     
     render() {
         return (
-                <table>
+                <StyledTable>
                     <thead>
-                        <tr>
-                            <td>Name</td>
-                            <td>Cost</td>
-                            <td>Delete</td>
-                        </tr>
+                        <StyledHeadingRow> 
+                            <StyledHeadingCell>Name</StyledHeadingCell>
+                            <StyledHeadingCell>Cost</StyledHeadingCell>
+                            <StyledHeadingCell>Delete</StyledHeadingCell>
+                        </StyledHeadingRow>
                     </thead>
                     <tbody>
                         { this.state.expenses.map(val => 
                             <tr key={val._id}>
-                                <td>{val.name}</td>
-                                <td>{val.cost.$numberDecimal}</td>
-                                <td><button onClick={() => this.deleteById(val._id)}>X</button></td>
+                                <StyledCell>{val.name}</StyledCell>
+                                <StyledCell>{val.cost.$numberDecimal}</StyledCell>
+                                <StyledCell><button onClick={() => this.deleteById(val._id)}>X</button></StyledCell>
                             </tr>
                         ) }
                     </tbody>
-                </table>
+                </StyledTable>
         )
     }
 
